abc1f6155fbbe5290fad072622691af98839f73e
Kristian Soto <kroyxlab@gmail.com>
2022-06-21 15:42:33 -0400

    Refaru la komandon "sendu"
    la komando sendu-eldonajxon esti forigata

--- komandoj/sendu.js
+++ komandoj/sendu.js
@@ -5,26 +5,25 @@ const wait = require('node:timers/promises').setTimeout;
 module.exports = {
   data: new SlashCommandBuilder()
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
-    .setName('sendu-eldonajxon')
+    .setName('sendu')
     .setDescription('Sendu eldonaĵon per la roboto')
+    .addStringOption((eblo) => eblo
+      .setName('teksto')
+      .setDescription('Skribu priskribon de la eldonaĵo'))
     .addAttachmentOption((eblo) => eblo
       .setName('eldonajxo')
-      .setDescription('Elŝutu eldonaĵon')
-      .setRequired(true))
-    .addStringOption((eblo) => eblo
-      .setName('priskribo')
-      .setDescription('Skribu priskribon de la eldonaĵo')),
+      .setDescription('Elŝutu eldonaĵon')),
 
   async execute(interaction) {
     const eblo = interaction.options;
     const cxiKanalo = interaction.channel;
     // Obtenu la eblojn
     const eldonajxo = eblo.getAttachment('eldonajxo');
-    const priskribo = eblo.getString('priskribo');
+    const teksto = eblo.getString('teksto');
 
     // Se la ebloj estis enmetataj, sendu ilin al la kanalo
-    cxiKanalo.send(eldonajxo.url);
-    if (priskribo !== null) cxiKanalo.send(priskribo);
+    if (eldonajxo !== null) cxiKanalo.send(eldonajxo.url);
+    cxiKanalo.send(teksto);
 
     // sendu kaj forigu mesaĝon
     interaction.deferReply();
